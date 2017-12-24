c0c91583517305b499ab39afa3e7b5b63b7bba93
kllmctrl <kllmctrl@gmail.com>
2017-12-24 11:42:33 +0800

    echo server

--- echo/echo/Program.cs
+++ echo/echo/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
 
 namespace echo
 {
@@ -7,6 +9,34 @@ namespace echo
 		public static void Main (string[] args)
 		{
 			Console.WriteLine ("Hello World!");
+
+			//Socket
+			Socket listenfd = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+			//Bind
+			IPAddress ipAdr = IPAddress.Parse("127.0.0.1");
+			IPEndPoint ipEp = new IPEndPoint (ipAdr, 1234);
+			listenfd.Bind (ipEp);
+
+			//Listen
+			listenfd.Listen(0);
+			Console.WriteLine ("[Sever] Start sucess");
+
+			while(true){
+				//Accept
+				Socket connfd = listenfd.Accept();
+				Console.WriteLine ("[Server] Accept");
+
+				//Recv
+				byte[] readBuff = new byte[1024];
+				int count = connfd.Receive (readBuff);
+				string str = System.Text.Encoding.UTF8.GetString (readBuff, 0, count);
+				Console.WriteLine ("[Server receive]"+str);
+
+				//Send
+				byte[] bytes = System.Text.Encoding.Default.GetBytes("serv echo" + str);
+				connfd.Send (bytes);
+			}
 		}
 	}
 }
